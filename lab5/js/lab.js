1e8f90dfe27a542ded8001a595540216ba277433
cwong5768 <cwong5768@gmail.com>
2025-04-22 18:15:26 -0700

    Lab 5 Submission
    - Added Task 1 variables to javascript file
    - Added JS output to HTML file and image of the car
    - Filled out the challenges, problems, and results section

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Holds variables created in Task 1
+// Author: Cindy Wong <cwong788@ucsc.edu>
+// Date: April 22 2024
 
-// Constants
+// Define Variables
+make = "Toyota";
+model = "Camry";
+color = "Beige";
+year = 2005;
 
-// Functions
+// Calculate
+age = 2025 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
