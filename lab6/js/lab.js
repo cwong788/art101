d497665ab1f0b4c2ece32f1e50cd01e9a288b8a8
cwong5768 <cwong5768@gmail.com>
2025-04-24 18:36:03 -0700

    Submission for Lab 6
    - Added variables to Javascript
    - Outputted them in HTML
    - Added an image and filled out challenge, problems, and reflection sections

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Holds variables created from Task 1
+// Author: Cindy Wong
+// Date: 24 April 2025
 
-// Constants
+// Define Variables
+myTransport = ["walking", "riding a bus", "driving a car"];
+myMainRide = {
+  make: "Toyota",
+  model: "Camry",
+  color: "Beige",
+  year: 2005,
+  age: function() {
+    return 2025 - year;
+  }
+}
 
-// Functions
+// Output
+document.writeln("How I Get Around: " + myTransport[0] + ", " + myTransport[1] + ", " + myTransport[2] + "<br>");
+document.writeln("My Main Ride: <pre>", JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
