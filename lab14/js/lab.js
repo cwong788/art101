d56b1cdb937bcd51e5e30bed1e3595359d06260b
cwong5768 <cwong5768@gmail.com>
2025-05-27 19:09:39 -0700

    Lab 14 Creation
    - created lab 14 folder and stuff

---
+++ lab14/js/lab.js
@@ -0,0 +1,15 @@
+// lab.js - Javascript file for Lab 14
+// Author: Cindy Wong
+// Date: 27 May 2025
+
+// Variables
+
+// Functions
+
+
+// Ask for user's name and sort it
+function main() {
+}
+
+// let's get this party started
+main();
