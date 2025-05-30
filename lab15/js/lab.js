281042240af126e7aec8a7fb29e520aac26e0864
cwong5768 <cwong5768@gmail.com>
2025-05-29 19:17:50 -0700

    Task X Lab 15
    Task X Lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,12 +1,12 @@
-// lab.js - Javascript file for Lab 14
+// lab.js - Javascript file for Lab 15
 // Author: Cindy Wong
-// Date: 27 May 2025
+// Date: 29 May 2025
 
 // Variables
 
 // Functions
-// click listener for button
-$("#activate").click(function(){
+// click listener for Chuck Norris joke
+$("#joke").click(function(){
     // Using the core $.ajax() method
     $.ajax({
         // The URL for the request (from the api docs)
@@ -21,6 +21,30 @@ $("#activate").click(function(){
             console.log(data);
             let result = data.value;
             $("#output").append('<p>' + result + '</p>');
+        },
+        // What we do if the api call fails
+        error: function (jqXHR, textStatus, errorThrown) { 
+            console.log("Error:", textStatus, errorThrown);
+        }
+    })
+});
+
+// click listener for Nasa's Photo of the Day button
+$("#nasa").click(function(){
+    // Using the core $.ajax() method
+    $.ajax({
+        // The URL for the request (from the api docs)
+        url: "https://api.nasa.gov/planetary/apod?api_key=Kapi8KVznDOGhCGvf1r3iAEstyKhfAtl6gfdvl9B",
+        // Whether this is a POST or GET request
+        type: "GET",
+        // The type of data we expect back
+        dataType : "json",
+        // What do we do when the api call is successful
+        success: function(data) {
+            console.log("Success");
+            console.log(data);
+            let result = data.url;
+            $("#output").append("<img class=photo src=" + data.url + ">");
         },
         // What we do if the api call fails
         error: function (jqXHR, textStatus, errorThrown) { 
